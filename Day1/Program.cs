ec12f375507b68e9e214444da3107c2c279207bf
leader <123@163.com>
2026-08-03 20:18:11 +0800

    visual stuio(day1)

---
+++ Day1/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace 试试
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1
+                int a = 12,b = 11, c = a + b;
+                Console.WriteLine(c);
+            //2
+                double huashidu ,sheshidu,aaa;
+                Console.Write("当前华氏度为：");
+                string huashidu2 = Console.ReadLine();
+                var huashidu3 = double.Parse(huashidu2);
+                double sheshidu0 =( huashidu3 * 9.0/5-32) ;
+                sheshidu=Math.Truncate(sheshidu0*1000)/1000;
+                Console.WriteLine("摄氏度为{0}°，华氏度为{1}°",sheshidu,huashidu3);
+            //4
+                Console.Write("当前救援小时数为：");
+                string hours = Console.ReadLine();
+                int real_hours = int.Parse(hours);
+                int day = real_hours / 24;
+                int h = real_hours - day * 24;
+                Console.WriteLine("救援天数为{0}天，{1}小时。",day, h);
+
+
+
+
+        }
+    }
+    
+}
---
+++ Day2/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Day2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> student = new List<string>()
+{
+                "张三",
+                "李四",
+                "王五"
+};
+        }
+    }
+}
